bd1d7829d521d8a9e6047ffb1f33269c73a6cf57
Samuel Seo <ngogiaphat0802@gmail.com>
2023-07-08 07:55:54 +0700

    style code

--- JS/Algorithm/Graph/BellmanFord/BellmanFordNoNegativeCycle.js
+++ JS/Algorithm/Graph/BellmanFord/BellmanFordNoNegativeCycle.js
@@ -1,41 +1,41 @@
 class Edge {
-    constructor(start, end, weight){
-      this.start = start;
-      this.end = end;
-      this.weight = weight;
-    }
+	constructor(start, end, weight){
+		this.start = start;
+		this.end = end;
+		this.weight = weight;
+	}
 }
 class Graph {
-    constructor(vertices){
-        this.vertices = vertices;
-        this.edges = [];
-    }
-    addEdge(start, end, weight){
-        const edge = new Edge(start, end, weight);
-        this.edges.push(edge);
-    }
-    bellmanFord(start){
-        const dist = {};
-        const prev = {};
-        for (let i = 0; i < this.vertices.length; i++){
-            dist[this.vertices[i]] = Infinity;
-            prev[this.vertices[i]] = null;
-        }
-        dist[start] = 0;
-        for (let i = 0; i < this.vertices.length - 1; i++){
-            for (let j = 0; j < this.edges.length; j++){
-                const edge = this.edges[j];
-                const u = edge.start;
-                const v = edge.end;
-                const weight = edge.weight;
-                if (dist[u] !== Infinity && dist[u] + weight < dist[v]){
-                    dist[v] = dist[u] + weight;
-                    prev[v] = u;
-                }
-            }
-        }
-        return {dist, prev};
-    }
+	constructor(vertices){
+		this.vertices = vertices;
+		this.edges = [];
+}
+	addEdge(start, end, weight){
+		const edge = new Edge(start, end, weight);
+		this.edges.push(edge);
+	}
+	bellmanFord(start){
+		const dist = {};
+		const prev = {};
+		for (let i = 0; i < this.vertices.length; i++){
+			dist[this.vertices[i]] = Infinity;
+			prev[this.vertices[i]] = null;
+		}
+		dist[start] = 0;
+		for(let i = 0; i < this.vertices.length - 1; i++){
+			for(let j = 0; j < this.edges.length; j++){
+				const edge = this.edges[j];
+				const u = edge.start;
+				const v = edge.end;
+				const weight = edge.weight;
+				if(dist[u] !== Infinity && dist[u] + weight < dist[v]){
+					dist[v] = dist[u] + weight;
+					prev[v] = u;
+				}
+			}
+		}
+		return {dist, prev};
+	}
 }
 const vertices = ['A', 'B', 'C', 'D', 'E'];
 const graph = new Graph(vertices);
