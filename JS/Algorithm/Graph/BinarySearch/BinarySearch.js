ac2bfe4f3d045ba7101fd905f80b970bf3f6dc45
Samuel Seo <ngogiaphat0802@gmail.com>
2023-07-08 07:59:47 +0700

    Binary Search bu JS

--- JS/Algorithm/Graph/BinarySearch/BinarySearch.js
+++ JS/Algorithm/Graph/BinarySearch/BinarySearch.js
@@ -3,7 +3,7 @@ function binarySearch(arr, target){
     let right = arr.length - 1;
     while (left <= right){
       let mid = Math.floor((left + right) / 2);
-      if (arr[mid] === target){
+      if(arr[mid] === target){
         return mid;
       } 
       else if (arr[mid] < target){
@@ -16,7 +16,6 @@ function binarySearch(arr, target){
     return -1;   
     //When target is not found in array arr, return -1
   }
-  
   //Example of using the algorithm Binary Search
   const arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91];
   const target = 23;
