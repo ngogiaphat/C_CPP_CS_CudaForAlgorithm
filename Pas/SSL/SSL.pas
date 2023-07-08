f5d1c9eb9969a993b08c0fa1b6b363306c05291b
Samuel Seo <ngogiaphat0802@gmail.com>
2023-07-08 10:54:04 +0700

    style code

--- Pas/SSL/SSL.pas
+++ Pas/SSL/SSL.pas
@@ -1,31 +1,28 @@
 program SSLExample;
-type
-  SSLConnection = record
-    // Define properties and methods for SSL connection here
-  end;
-function CreateSSLConnection(host: string; port: integer): SSLConnection;
-begin
-  // Implement SSL connection creation logic here
-end;
-procedure ConnectSSL(connection: SSLConnection);
-begin
-  // Implement SSL connection establishment logic here
-end;
-procedure SendMessageSSL(connection: SSLConnection; message: string);
-begin
-  // Implement logic to send message over SSL connection here
-end;
-
-procedure CloseSSL(connection: SSLConnection);
-begin
-  // Implement SSL connection closing logic here
-end;
-var
-  sslConn: SSLConnection;
-begin
-  // Usage example
-  sslConn := CreateSSLConnection('example.com', 443);
-  ConnectSSL(sslConn);
-  SendMessageSSL(sslConn, 'This is a test message');
-  CloseSSL(sslConn);
-end.
+    type SSLConnection = record
+        // Define properties and methods for SSL connection here
+    end;
+    function CreateSSLConnection(host: string; port: integer): SSLConnection;
+        begin
+        // Implement SSL connection creation logic here
+        end;
+    procedure ConnectSSL(connection: SSLConnection);
+        begin
+        // Implement SSL connection establishment logic here
+        end;
+    procedure SendMessageSSL(connection: SSLConnection; message: string);
+        begin
+        // Implement logic to send message over SSL connection here
+        end;
+    procedure CloseSSL(connection: SSLConnection);
+        begin
+        // Implement SSL connection closing logic here
+        end;
+    var sslConn: SSLConnection;
+        begin
+            // Usage example
+            sslConn := CreateSSLConnection('example.com', 443);
+            ConnectSSL(sslConn);
+            SendMessageSSL(sslConn, 'This is a test message');
+            CloseSSL(sslConn);
+        end.
