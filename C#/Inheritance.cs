d7cc6f952d65bf2cff404b92264717080a3bd237
Samuel Seo <ngogiaphat0802@gmail.com>
2023-08-03 09:09:36 +0700

    style code

--- C#/Inheritance.cs
+++ C#/Inheritance.cs
@@ -1,25 +1,19 @@
-class Vehicle  // base class (parent) 
-{
-  public string brand = "Ford";  // Vehicle field
-  public void honk()             // Vehicle method 
-  {                    
-    Console.WriteLine("Tuut, tuut!");
-  }
+class Vehicle {
+    public string brand = "Ford";  // Vehicle field
+    public void honk(){
+        Console.WriteLine("Tuut, tuut!");
+    }
 }
-class Car : Vehicle  // derived class (child)
-{
-  public string modelName = "Mustang";  // Car field
+class Car : Vehicle {
+    public string modelName = "Mustang";  // Car field
 }
-class Program
-{
-  static void Main(string[] args)
-  {
-    // Create a myCar object
-    Car myCar = new Car();
-    // Call the honk() method (From the Vehicle class) on the myCar object
-    myCar.honk();
-
-    // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
-    Console.WriteLine(myCar.brand + " " + myCar.modelName);
-  }
+class Program {
+    static void Main(string[] args){
+        // Create a myCar object
+        Car myCar = new Car();
+        // Call the honk() method (From the Vehicle class) on the myCar object
+        myCar.honk();
+        // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+        Console.WriteLine(myCar.brand + " " + myCar.modelName);
+    }
 }
