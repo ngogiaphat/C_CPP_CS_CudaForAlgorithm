310d0835b5dde72773d3cfa4cb8c8be2884eebb1
Samuel Seo <ngogiaphat0802@gmail.com>
2023-08-03 12:40:35 +0700

    style code

--- C#/Algorithm/Graph/DynamicProgramming/DynamicProgramming.cs
+++ C#/Algorithm/Graph/DynamicProgramming/DynamicProgramming.cs
@@ -4,14 +4,14 @@ public class Fibonacci {
         int[] fib = new int[n + 1];
         fib[0] = 0;
         fib[1] = 1;
-        for (int i = 2; i <= n; ++i){
+        for(int i = 2; i <= n; ++i){
             fib[i] = fib[i - 1] + fib[i - 2];
         }
         return fib[n];
     }
 }
 public class Program {
-    static void Main(string[] args) {
+    static void Main(string[] args){
         int n = 10;
         int result = Fibonacci.GetNthFibonacci(n);
         Console.WriteLine("The {0}th Fibonacci number is {1}", n, result);
