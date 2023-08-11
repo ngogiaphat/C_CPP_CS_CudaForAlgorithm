4fce4835837095b4d2f560b657c3db877543b2de
Samuel Seo <ngogiaphat0802@gmail.com>
2023-08-11 23:24:22 +0700

    change indent size

--- C#/method.cs
+++ C#/method.cs
@@ -1,6 +1,6 @@
 private void MyTask(string[] files){
-  foreach (string file in files){
-    // a time consuming operation with a file (compression, encryption etc.)
-    Thread.Sleep(1000);
-  }
+    foreach (string file in files){
+        // a time consuming operation with a file (compression, encryption etc.)
+        Thread.Sleep(1000);
+    }
 }
