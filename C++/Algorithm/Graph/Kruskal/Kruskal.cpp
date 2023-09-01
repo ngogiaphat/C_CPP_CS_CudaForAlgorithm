f31fc256250466f75d71734f9a6cb09f8b1ab4f6
Samuel Seo <ngogiaphat0802@gmail.com>
2023-09-02 01:13:39 +0700

    style code

--- C++/Algorithm/Graph/Kruskal/Kruskal.cpp
+++ C++/Algorithm/Graph/Kruskal/Kruskal.cpp
@@ -6,13 +6,13 @@ struct Edge{
 };
 bool cmp(Edge a, Edge b){
     return a.w < b.w;
-}
+};
 int find(int i){
     if(parent[i] == i){
         return i;
-    }
+    };
     return parent[i] = find(parent[i]);
-}
+};
 void Union(int x, int y){
     int rootX = find(x);
     int rootY = find(y);
@@ -22,18 +22,18 @@ void Union(int x, int y){
     else if(rankArr[rootX] > rankArr[rootY]){
         parent[rootY] = rootX;
     }
-    else{
+    else {
         parent[rootX] = rootY;
         rankArr[rootY]++;
-    }
-}
+    };
+};
 vector<Edge> Kruksal(vector<Edge> edges, int n){
     vector<Edge> MST;
     sort(edges.begin(), edges.end(), cmp);
     for(int i = 0; i < n; i++){
         parent[i] = i;
         rankArr[i] = 0;
-    }
+    };
     int cnt = 0;
     for(auto edge : edges){
         int u = edge.u, v = edge.v, w = edge.w;
@@ -41,19 +41,27 @@ vector<Edge> Kruksal(vector<Edge> edges, int n){
             cnt++;
             MST.push_back({u, v, w});
             Union(u, v);
-        }
+        };
         if(cnt == n - 1){
             break;
-        }
-    }
+        };
+    };
     return MST;
-}
+};
 int main(){
     int n = 5;
-    vector<Edge> edges = {{0, 1 ,6}, {0, 3 ,1}, {1, 2 ,5}, {1, 3, 2}, {1, 4, 2}, {2, 4, 1}, {3, 4, 1}};
+    vector<Edge> edges = {
+        {0, 1 ,6},
+        {0, 3 ,1},
+        {1, 2 ,5},
+        {1, 3, 2},
+        {1, 4, 2},
+        {2, 4, 1},
+        {3, 4, 1}
+    };
     vector<Edge> MST = Kruksal(edges, n);
     for(auto edge : MST){
         cout << edge.u << " - " << edge.v << " : " << edge.w << endl;
-    }
+    };
     return 0;
-}
+};
