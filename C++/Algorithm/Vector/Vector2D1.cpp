c2eafcdbf4b6ed264f487b3dfe3227f88172d94f
Samuel Seo <ngogiaphat0802@gmail.com>
2023-10-18 21:41:33 +0700

    style code

--- C++/Algorithm/Vector/Vector2D1.cpp
+++ C++/Algorithm/Vector/Vector2D1.cpp
@@ -1,22 +1,21 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
 #define ROW 4
 #define COL 5
-int main()
-{
+int main(){
 	vector<vector<int> > vec;
 	int num = 10;
-	for (int i = 0; i < ROW; i++) {
+	for(int i = 0; i < ROW; i++){
 		vector<int> v1;
-		for (int j = 0; j < COL; j++) {
+		for(int j = 0; j < COL; j++){
 			v1.push_back(num);
 			num += 5;
 		}
 		vec.push_back(v1);
 	}
-	for (int i = 0; i < vec.size(); i++) {
-		for (int j = 0; j < vec[i].size(); j++)
+	for(int i = 0; i < vec.size(); i++){
+		for(int j = 0; j < vec[i].size(); j++)
 			cout << vec[i][j] << " ";
 		cout << endl;
 	}
--- C++/Algorithm/Vector/Vector2D2.cpp
+++ C++/Algorithm/Vector/Vector2D2.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main()
-{
-	vector<vector<int> > vec{ { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+int main(){
+	vector<vector<int> > vec{
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9},
+    };
 	vec[2].pop_back();
 	vec[1].pop_back();
-	for (int i = 0; i < 3; i++) {
-		for (auto it = vec[i].begin(); it != vec[i].end(); it++)
-			cout << *it << " ";
+	for(int i = 0; i < 3; i++){
+		for(auto it = vec[i].begin(); it != vec[i].end(); it++){
+            cout << *it << " ";
+        }
 		cout << endl;
 	}
 	return 0;
